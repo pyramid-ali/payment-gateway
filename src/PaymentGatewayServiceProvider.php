618abedfe59b919e39c9fc889dbaeef11213b550
Ali Shabani <pryamidalish@gmail.com>
2020-03-11 03:49:39 +0330

    added publish config

--- src/PaymentGatewayServiceProvider.php
+++ src/PaymentGatewayServiceProvider.php
@@ -7,8 +7,19 @@ use Illuminate\Support\ServiceProvider;
 class PaymentGatewayServiceProvider extends ServiceProvider
 {
 
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/payment-gateway.php' => config_path('payment-gateway.php'),
+        ], 'payment-gateway-config');
+    }
+
     public function register()
     {
+        $this->publishes([
+            __DIR__.'/../config/airlock.php' => config_path('airlock.php'),
+        ], 'airlock-config');
+
         $this->app->bind(PaymentGateway::class, function ($app) {
             return new PaymentGateway($app);
         });
